951bd968f558fa16346ba6e281885110aa900ee2
ACNiC03 <ahmetnicanci@hotmail.com>
2021-12-15 14:20:46 +0100

    List vars afgemaakt

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import json
 
 with open('steam.json', 'r') as file:
-    data = []
     oldData = json.load(file)
 
-# Variabele maken voor de informatie in json file
+
+# Vars for json file
 appid = [x['appid'] for x in oldData]
 name = [x['name'] for x in oldData]
 release_date = [x['release_date'] for x in oldData]
-# ...
+english = [x['english'] for x in oldData]
+developer = [x['developer'] for x in oldData]
+publisher = [x['publisher'] for x in oldData]
+platforms = [x['platforms'] for x in oldData]
+required_age = [x['required_age'] for x in oldData]
+categories = [x['categories'] for x in oldData]
+genres = [x['genres'] for x in oldData]
+steamspy_tags = [x['steamspy_tags'] for x in oldData]
+achievements = [x['achievements'] for x in oldData]
+positive_ratings = [x['positive_ratings'] for x in oldData]
+negative_ratings = [x['negative_ratings'] for x in oldData]
+average_playtime = [x['average_playtime'] for x in oldData]
+median_playtime = [x['median_playtime'] for x in oldData]
+owners = [x['owners'] for x in oldData]
+price = [x['price'] for x in oldData]
+
+
+
 
 def sort(list):
     return sorted(list)
-
